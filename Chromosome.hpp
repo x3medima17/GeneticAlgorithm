85d129ba48f078fc011dd23679dd2e0f21ca44ae
dumitru <dimasavva17@gmail.com>
2017-04-10 16:45:18 +0300

    formula GA

--- Chromosome.hpp
+++ Chromosome.hpp
@@ -10,11 +10,11 @@
 class Chromosome {
 public:
     std::vector<Gene> genes;
+    size_t acc;
 
+    Chromosome(const std::vector<double> &V, size_t acc);
 
-    Chromosome(const std::vector<double> &V);
-
-    Chromosome(const std::vector<Nucleotide> &V);;
+    Chromosome(const std::vector<bool> &V, size_t acc);
 
     std::vector<double> get_vect() const {
         std::vector<double> out;
@@ -24,7 +24,7 @@ public:
         return out;
     }
 
-    std::vector<Nucleotide> get_genome() const;
+    std::vector<bool> get_genome() const;
 
     void print() const;
 
@@ -32,28 +32,22 @@ public:
 };
 
 
-Chromosome::Chromosome(const std::vector<double> &V) {
+Chromosome::Chromosome(const std::vector<double> &V, size_t acc): acc{acc} {
     for (const auto &item : V)
-    {
-        std::vector<Nucleotide> curr_gene(sizeof(double)*4);
-
-        for (int i = 0; i < sizeof(double) * 8; i += 2) {
-            auto n = Nucleotide(bit_ops::read_bit(item, i + 1), bit_ops::read_bit(item, i));
-            curr_gene.at(curr_gene.size()-i/2-1) = n;
-        }
-        genes.push_back(Gene(curr_gene));
-    }
-    auto Vp = get_vect();
+        genes.push_back(Gene(item, acc));
 }
 
-Chromosome::Chromosome(const std::vector<Nucleotide> &V) {
-    std::vector<Nucleotide> curr_gene;
+Chromosome::Chromosome(const std::vector<bool> &V, size_t acc) : acc{acc}  {
+    assert(acc > 0);
+    assert(V.size() % acc == 0);
+
+    std::vector<bool> curr_gene;
     int k = 0;
     for(const auto& nuc : V)
     {
         curr_gene.push_back(nuc);
         k++;
-        if(k == 32)
+        if(k == acc)
         {
             k = 0;
             genes.push_back(Gene(curr_gene));
@@ -62,8 +56,8 @@ Chromosome::Chromosome(const std::vector<Nucleotide> &V) {
     }
 }
 
-std::vector<Nucleotide> Chromosome::get_genome() const {
-    std::vector<Nucleotide> out;
+std::vector<bool> Chromosome::get_genome() const {
+    std::vector<bool> out;
     for(const auto& gene : genes)
         out.insert(out.end(), gene.nucleotides.begin(), gene.nucleotides.end());
     return out;
--- OrganismFactory.hpp
+++ OrganismFactory.hpp
@@ -9,68 +9,95 @@
 #include <assert.h>
 #include "Organism.hpp"
 
-template<class T>
 class OrganismFactory {
 public:
     const std::vector<std::pair<double, double>> Limits;
     const double MutationRate;
     const size_t Dimension, CrossLoci;
+    const size_t acc;
 
     mutable std::mt19937 generator;
 
     OrganismFactory(const std::vector<std::pair<double, double>> &Limits,
                     const double MutationRate, const size_t CrossLoci,
-                    const size_t Dimenstion);
+                    const size_t Dimenstion, size_t acc);
 
-    virtual T generate() const;
+    virtual Organism generate() const;
 
-    virtual std::pair<T, T> crossover(const T &parent1,
-                                      const T &parent2) const;;
+    virtual std::pair<Organism, Organism> crossover(const Organism &parent1,
+                                      const Organism &parent2) const;;
 
-    virtual double compute_fitness(const T &org) const;
+    virtual double normalize(double x, double L, double R) const;
+
+    virtual double compute_fitness(const Organism org) const = 0;
 
 };
 
-template<class T>
-OrganismFactory<T>::OrganismFactory(const std::vector<std::pair<double, double>> &Limits, const double MutationRate,
-                                    const size_t CrossLoci, const size_t Dimenstion) :
+OrganismFactory::OrganismFactory(const std::vector<std::pair<double, double>> &Limits, const double MutationRate,
+                                    const size_t CrossLoci, const size_t Dimenstion, size_t acc) :
         Limits(Limits),
         MutationRate(MutationRate),
         CrossLoci(CrossLoci),
-        Dimension(Dimenstion) {
+        Dimension(Dimenstion),
+        acc(acc) {
 
     assert(Dimension == Limits.size());
     assert(CrossLoci % 2 == 0);
 }
 
-template <class Organism>
-Organism OrganismFactory<Organism>::generate() const {
-    std::vector<double> curr(Dimension);
+double OrganismFactory::normalize(double x, double L, double R) const {
+    return L + x*(R-L);
+}
+
+Organism OrganismFactory::generate() const {
+    std::vector<double> V(Dimension);
     size_t idx = 0;
-   
-    for (auto &val : curr) {
-		std::uniform_real_distribution<double> value_dist{ Limits.at(idx).first,
-			Limits.at(idx).second };
-		idx++;
+
+    for (auto& val: V) {
+        auto L = Limits.at(idx).first;
+        auto R = Limits.at(idx).second;
+
+        std::uniform_real_distribution<double> value_dist{L, R};
         val = value_dist(generator);
+        val = (val - L)/(R-L);
+        idx++;
     }
-    Organism o(curr);
+
+    Organism o{Chromosome(V, acc)};
     o.set_fitness(compute_fitness(o));
     return o;
 }
 
-template<class Organism>
-std::pair<Organism, Organism> OrganismFactory<Organism>::crossover(const Organism &parent1, const Organism &parent2) const {
+std::pair<Organism, Organism> OrganismFactory::crossover(const Organism &parent1, const Organism &parent2) const {
+    std::vector<bool> a(parent1.DNA.get_genome());
+    std::vector<bool> b(parent2.DNA.get_genome());
+    std::vector<size_t> loci(CrossLoci);
 
-    auto res = Organism::cross_over(parent1, parent2, CrossLoci, MutationRate, generator);
-    res.first.set_fitness(compute_fitness(res.first));
-    res.second.set_fitness(compute_fitness(res.second));
-    return res;
+    std::uniform_int_distribution<size_t> nucleotide_index_dist{0,parent2.DNA.get_genome().size() -1};
+    std::uniform_real_distribution<double> mutation_dist{0, 1};
+
+    for(auto& locus : loci)
+        locus = nucleotide_index_dist(generator);
+    loci.at(0) = 0;
+    for(size_t i = 0; i < CrossLoci - 1; i+=2)
+        for(size_t j = loci.at(i); j<loci.at(i+1); j++)
+            std::swap(a.at(j), b.at(j));
+
+    //mutation_dist(generator) < 0.5 returns bit index [0,1]
+    if(mutation_dist(generator) > MutationRate)
+        a.at(nucleotide_index_dist(generator)) = mutation_dist(generator) < 0.5;
+
+    if(mutation_dist(generator) > MutationRate)
+        b.at(nucleotide_index_dist(generator)) = mutation_dist(generator) < 0.5;
+
+    Organism child1{ Chromosome(a, acc) };
+    Organism child2{ Chromosome(b, acc) };
+
+    child1.set_fitness(compute_fitness(child1));
+    child2.set_fitness(compute_fitness(child2));
+
+
+    return std::make_pair(child1,child2);
 }
 
-
-template<class Organism>
-double OrganismFactory<Organism>::compute_fitness(const Organism &org) const {
-    return org.compute_fitness(Limits);
-}
 
